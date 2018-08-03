39c618d47feeaf4021b56cb6ed2f5c3309f70d8c
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-08-03 12:40:41 -0400

    start on sets

---
+++ Flashcards/Controllers/ViewSetController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Flashcards.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Flashcards.Controllers {
+    public class ViewSetController : Controller {
+
+        public ViewSetController(IHostingEnvironment environment){
+            HostEnv = environment;
+        }
+
+        private IHostingEnvironment HostEnv { get; set; }
+
+        public IActionResult Index(string username, string setname) {
+
+            string filepath = Path.Combine(HostEnv.WebRootPath, $"sets/{username}/{setname}.json");
+            CardSet set = null;
+            if(System.IO.File.Exists(filepath)){
+                set = CardSet.BuildFromJson(System.IO.File.ReadAllText(filepath));
+            }
+
+            return View(set);
+        }
+    }
+}
---
+++ Flashcards/Models/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Flashcards.Models {
+    public class Card {
+        public string QuestionText {
+            get;
+            set;
+        }
+        public string QuestionImageUrl {
+            get;
+            set;
+        }
+        public string AnswerText {
+            get;
+            set;
+        }
+        public string AnswerImageUrl {
+            get;
+            set;
+        }
+    }
+}
