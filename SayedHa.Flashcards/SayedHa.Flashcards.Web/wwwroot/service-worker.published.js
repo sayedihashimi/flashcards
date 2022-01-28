24338a5cc55ec0cb5f097aaa6cf49af3f8dfa0f6
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2022-01-28 11:38:32 -0500

    use string instead of img for letters/numbers

--- SayedHa.Flashcards/SayedHa.Flashcards.Web/wwwroot/service-worker.published.js
+++ SayedHa.Flashcards/SayedHa.Flashcards.Web/wwwroot/service-worker.published.js
@@ -6,6 +6,10 @@ self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
 self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
+// changing this version number will force the service worker to be updated
+// see: https://stackoverflow.com/a/66783449/105999
+const swVersion = "0.0.1";
+
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
 const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
