a05f3ac4cb403099177b43a4ea8794094d14cc21
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2022-11-26 00:55:58 -0500

    more progress on clicknumber game

---
+++ SayedHa.Flashcards/SayedHa.Flashcards.Web/ClickNumberItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace SayedHa.Flashcards.Web {
+    public class ClickNumberItem {
+        public int Number { get; set; }
+        public bool Enabled { get; set; } = true;
+        public bool CurrentlySelected { get; set; } = false;
+        public bool IsCompleted { get; set; } = false;
+    }
+
+    public class SpecialCharItem {
+        public SpecialCharItem(string specialChar) {
+            Debug.Assert(!string.IsNullOrEmpty(specialChar));
+            SpecialChar = specialChar;
+        }
+        public string SpecialChar { get; set; }
+        public bool Enabled { get; set; } = true;
+    }
+
+}
